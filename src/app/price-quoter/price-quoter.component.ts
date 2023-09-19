ddf6c9cf1f8cced96b727abbfece6a962b820778
joeyclapton <joeyclapton42@gmail.com>
2023-09-19 00:03:02 -0300

    fix: adjust input property

--- src/app/price-quoter/price-quoter.component.ts
+++ src/app/price-quoter/price-quoter.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { IPriceQuote } from '../interfaces/iprice.quote'
+import { Component, EventEmitter, Output, Input, ElementRef, OnChanges, SimpleChanges } from '@angular/core';
+import { IPriceQuote } from '../interfaces/iprice.quote';
 import { interval } from 'rxjs';
 
 @Component({
@@ -7,18 +7,55 @@ import { interval } from 'rxjs';
   templateUrl: './price-quoter.component.html',
   styleUrls: ['./price-quoter.component.sass']
 })
-export class PriceQuoterComponent {
-  priceQuote!: IPriceQuote;
+export class PriceQuoterComponent implements OnChanges {
+  priceQuote: IPriceQuote = {
+    stockSymbol: '',
+    lastPrice: 0
+  };
 
   @Output() lastPrice = new EventEmitter<IPriceQuote>();
+  @Input() eventType = '';
 
-  constructor() {
+  constructor(private element: ElementRef) { }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['eventType']) {
+      this.initializeEventHandler();
+    }
+  }
+
+  initializeEventHandler() {
+    if (this.eventType === 'bubbling') {
+      this.bubblingEventHandler();
+    } else if (this.eventType === 'emitter') {
+      this.emitterEventHandler();
+    }
+  }
+
+  emitterEventHandler() {
     interval(2000).subscribe(data => {
       this.priceQuote = {
-        stockSymbol: 'Meta',
+        stockSymbol: 'Meta 🔵',
         lastPrice: 100 * Math.random()
-      }
-    })
-
+      };
+      this.lastPrice.emit(this.priceQuote);
+    });
   }
-} 
+
+  bubblingEventHandler() {
+    setInterval(() => {
+      const value: IPriceQuote = {
+        stockSymbol: 'Apple 🍎',
+        lastPrice: 100 * Math.random()
+      };
+      this.priceQuote = value;
+
+      this.element.nativeElement.dispatchEvent(
+        new CustomEvent('lastPrice', {
+          detail: this.priceQuote,
+          bubbles: true,
+        })
+      );
+    }, 1000);
+  }
+}
